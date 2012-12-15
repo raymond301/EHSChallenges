c67a1bdeeb855f176578e592c90d762bcdc11e46
Raymond Moore <raymond.moore301@gmail.com>
2012-12-15 09:48:21 -0600

    adding a challenge

---
+++ challenge01.c
@@ -0,0 +1,14 @@
+/*
+ * FTC Programming Challenge #1
+ * There is only 1 issue here.
+ * If you try to compile the program the issue should be revealed.
+ */
+
+#pragma platform(Tetrix)
+
+task main()
+{
+	motor[motorA] = 50 // set motorA to power level 50 (forward, half speed)
+  motor[motorB] = 50 // Forward, half speed
+  wait1Msec(1000)
+}
